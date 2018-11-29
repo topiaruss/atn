718f677099c45ff0540fb6b682a9e02475db905a
russ.ferriday <russ.ferriday@gmail.com>
2018-11-29 22:35:20 +0000

    Add hypothesis fuzz testing, and demonstrate. <Note: Update dependencies.>

--- atn/rest/tests.py
+++ atn/rest/tests.py
@@ -1,14 +1,22 @@
-from django.test import TestCase
+from hypothesis import given
+from hypothesis.extra.django import TestCase
+from hypothesis.strategies import text, characters
 from rest_framework.test import APIClient
+
+# define the range of name strings that hypothesis will present. Ensure that none start or end with <space>
+# and that none is zero length.  This is used in @given()
+names = text(characters(max_codepoint=1000, blacklist_categories=('Cc', 'Cs')), min_size=1).map(
+    lambda x: x.strip()).filter(lambda s: len(s) > 0)
 
 
 class BasicApiTests(TestCase):
 
-    def test_create_client_and_retrieve(self):
+    @given(names, names)
+    def test_create_client_and_retrieve(self, client_name, client_address):
         client = APIClient()
 
         # create the first Client
-        params = {'name': 'first client', 'address': 'first street'}
+        params = {'name': client_name, 'address': client_address}
         response = client.post('/rest/clients/', params, format='json')
         assert response.status_code == 201
 
@@ -17,9 +25,6 @@ class BasicApiTests(TestCase):
         assert response.status_code == 200
 
         # check it's the same as the original parameters.
-        # (we did not put the url into params, but it comes back, so add it before the comparison).
+        # (we did not put the url into params, but it is returned, so add it before the comparison).
         params['url'] = 'http://testserver/rest/clients/1/'
         assert response.data == params
-
-
-
