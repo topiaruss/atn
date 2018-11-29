1bcc24fe0feb6c141f92c9bc695a79f6d48aad93
russ.ferriday <russ.ferriday@gmail.com>
2018-11-29 16:33:25 +0000

    First tests

--- atn/partners/tests.py
+++ atn/partners/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Client
 
-# Create your tests here.
+
+class ClientTestCase(TestCase):
+    def setUp(self):
+        Client.objects.create(name="firstClient", address="first street")
+        Client.objects.create(name="secondClient", address="second street")
+
+    def test_create_clients(self):
+        """I can create clients"""
+        first = Client.objects.get(name="firstClient")
+        second = Client.objects.get(name="secondClient")
+        assert first
+        assert second
+        
+    def test_exception_when_none(self):
+        with self.assertRaises(Client.DoesNotExist) as context:
+          third = Client.objects.get(name="xxx")
+
+
