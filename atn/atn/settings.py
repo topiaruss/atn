eaa2abb46680d5b733f0e3573ba0b261affb2530
Graham Owens <graham@atn.co.uk>
2018-11-29 15:16:45 +0000

    Initial config for GAE

--- atn/atn/settings.py
+++ atn/atn/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!$whghx!3+-#ov9hf3t_71$rm@nb$l*qr#_lfeg^snmw3&q9-@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
--- atn/manage.py
+++ atn/manage.py
@@ -3,6 +3,10 @@ import os
 import sys
 
 if __name__ == '__main__':
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    LIB_DIR = os.path.join(BASE_DIR, 'atn', 'libs')
+    sys.path.append(LIB_DIR)
+    #import pdb; pdb.set_trace()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'atn.settings')
     try:
         from django.core.management import execute_from_command_line
